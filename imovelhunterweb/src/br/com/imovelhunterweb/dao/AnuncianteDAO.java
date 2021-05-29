26b2288522778ceba9b9723de392f9131358910f
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-05-30 01:51:43 +0800

    Crud da tabela Anunciante

---
+++ imovelhunterweb/src/br/com/imovelhunterweb/dao/AnuncianteDAO.java
@@ -0,0 +1,9 @@
+package br.com.imovelhunterweb.dao;
+
+import java.io.Serializable;
+
+import br.com.imovelhunterweb.entitys.Anunciante;
+
+public interface AnuncianteDAO extends GenericDAO<Anunciante,Serializable> {
+
+}
---
+++ imovelhunterweb/src/br/com/imovelhunterweb/dao/imp/AnuncianteDAOImp.java
@@ -0,0 +1,15 @@
+package br.com.imovelhunterweb.dao.imp;
+
+import java.io.Serializable;
+
+import br.com.imovelhunterweb.dao.AnuncianteDAO;
+import br.com.imovelhunterweb.entitys.Anunciante;
+
+public class AnuncianteDAOImp extends GenericDAOImp<Anunciante,Serializable> implements AnuncianteDAO,Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3444869977638262537L;
+
+}
