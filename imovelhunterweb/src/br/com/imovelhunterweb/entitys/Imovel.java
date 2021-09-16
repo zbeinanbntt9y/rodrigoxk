5e3c8ba3055d8a6b8721a4ce248e136dc051b8a4
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-09-17 03:30:27 +0800

    Informações de endereço adicionados.

--- imovelhunterweb/src/br/com/imovelhunterweb/entitys/Imovel.java
+++ imovelhunterweb/src/br/com/imovelhunterweb/entitys/Imovel.java
@@ -67,6 +67,24 @@ public class Imovel implements Serializable{
 	@Column
 	private int numeroDeSalas;
 	
+	@Column
+	private String logradouro; 
+	
+	@Column
+	private String bairro;
+	
+	@Column
+	private String cidade;
+	
+	@Column
+	private String estado;
+	
+	@Column
+	private String CEP;
+	
+	@Column
+	private String pais;
+
 	@ManyToOne
 	private Anunciante anunciante;
 	
@@ -183,6 +201,54 @@ public class Imovel implements Serializable{
 		this.numeroDeSalas = numeroDeSalas;
 	}
 
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCEP() {
+		return CEP;
+	}
+
+	public void setCEP(String cEP) {
+		CEP = cEP;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+	
 	public Anunciante getAnunciante() {
 		return anunciante;
 	}
