46b96a407bce48ccceb6463a48d8af3ee6e7262b
zbeinanbntt9y <wanghongjun0104@yeah.net>
2021-10-12 13:51:53 +0800

    Fale Conosco

---
+++ imovelhunterweb/src/br/com/imovelhunterweb/beans/FaleConoscoBean.java
@@ -0,0 +1,95 @@
+package br.com.imovelhunterweb.beans;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+
+import br.com.imovelhunterweb.entitys.Anunciante;
+import br.com.imovelhunterweb.service.AnuncianteService;
+import br.com.imovelhunterweb.util.Navegador;
+import br.com.imovelhunterweb.util.PrimeUtil;
+import br.com.imovelhunterweb.util.UtilSession;
+
+@ManagedBean(name = "loginBean")
+@SessionScoped
+public class FaleConoscoBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2465262372102338614L;
+	private String nome;
+	private String email;
+	private String telefoneFixo;
+	private String telefoneCelular;
+	private String mensagem;
+	
+	public List<String> completeArea(String query) {
+        List<String> results = new ArrayList<String>();
+         
+        if(query.equals("PrimeFaces")) {
+            results.add("PrimeFaces Rocks!!!");
+            results.add("PrimeFaces has 100+ components.");
+            results.add("PrimeFaces is lightweight.");
+            results.add("PrimeFaces is easy to use.");
+            results.add("PrimeFaces is developed with passion!");
+        }
+        else {
+            for(int i = 0; i < 10; i++) {
+                results.add(query + i);
+            }
+        }
+         
+        return results;
+    }
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefoneFixo() {
+		return telefoneFixo;
+	}
+	public void setTelefoneFixo(String telefoneFixo) {
+		this.telefoneFixo = telefoneFixo;
+	}
+	public String getTelefoneCelular() {
+		return telefoneCelular;
+	}
+	public void setTelefoneCelular(String telefoneCelular) {
+		this.telefoneCelular = telefoneCelular;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+	
+	
+	}
